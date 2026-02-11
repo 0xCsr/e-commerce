0a3301b456cc2b574eb1c4850222d68fabdd4c5e
0xCsr <cesarmjunior2011@hotmail.com>
2026-02-11 20:45:38 -0300

    fix(Order): fix item list

--- src/main/java/com/cesar/ecommerce/order/Order.java
+++ src/main/java/com/cesar/ecommerce/order/Order.java
@@ -1,17 +1,20 @@
 package com.cesar.ecommerce.order;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import com.cesar.ecommerce.BaseEntity;
 import com.cesar.ecommerce.address.Address;
 import com.cesar.ecommerce.user.User;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,8 +33,8 @@ public class Order extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    // @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    // private List<OrderItem> items;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
